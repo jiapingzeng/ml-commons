f7fff4f73b2fa9470cdac91959c0c4d37caf6c2c
Jiaping Zeng <jpz@amazon.com>
2025-11-19 01:39:10 -0800

    AG-UI support in Agent Framework (#4347)
    * AG-UI events
    
    Signed-off-by: Jiaping Zeng <jpz@amazon.com>
    
    * convert AG-UI input to agent parameters
    
    Signed-off-by: Jiaping Zeng <jpz@amazon.com>
    
    * add AG-UI tool use
    
    Signed-off-by: Jiaping Zeng <jpz@amazon.com>
    
    * add AG-UI processing
    
    Signed-off-by: Jiaping Zeng <jpz@amazon.com>
    
    * add feature flag
    
    Signed-off-by: Jiaping Zeng <jpz@amazon.com>
    
    * address comments
    
    Signed-off-by: Jiaping Zeng <jpz@amazon.com>
    
    ---------
    
    Signed-off-by: Jiaping Zeng <jpz@amazon.com>
    
    Fix check for agent request
    
    Signed-off-by: Nathalie Jonathan <nathhjo@amazon.com>
    Signed-off-by: Jiaping Zeng <jpz@amazon.com>
    
    AGUI fixes (#4466)
    
    Signed-off-by: Jiaping Zeng <jpz@amazon.com>
    
    Pass hook registry to agui agent (#4526)
    
    * fix null tool input in streaming
    
    Signed-off-by: Jiaping Zeng <jpz@amazon.com>
    
    * test streaming from gradle
    
    Signed-off-by: Jiaping Zeng <jpz@amazon.com>
    
    * ignore tool calls after first during parallel tool use
    
    Signed-off-by: Jiaping Zeng <jpz@amazon.com>
    
    * store frontend/backend tool names to be used in system prompt
    
    Signed-off-by: Jiaping Zeng <jpz@amazon.com>
    
    * spotless apply
    
    Signed-off-by: Jiaping Zeng <jpz@amazon.com>
    
    * pass hook registry to agui agent
    
    Signed-off-by: Mingshi Liu <mingshl@amazon.com>
    
    * upgrade java in CI
    
    Signed-off-by: Mingshi Liu <mingshl@amazon.com>
    
    ---------
    
    Signed-off-by: Jiaping Zeng <jpz@amazon.com>
    Signed-off-by: Mingshi Liu <mingshl@amazon.com>
    Co-authored-by: Jiaping Zeng <jpz@amazon.com>
    Signed-off-by: Jiaping Zeng <jpz@amazon.com>
    
    fix tool result parsing in AGUI agent (#4537)
    
    * fix tool result parsing when using backend tool and frontend tool together
    
    Signed-off-by: Jiaping Zeng <jpz@amazon.com>
    
    * convert empty tool input to empty map
    
    Signed-off-by: Jiaping Zeng <jpz@amazon.com>
    
    * remove unnecessary fallback logic
    
    Signed-off-by: Jiaping Zeng <jpz@amazon.com>
    
    ---------
    
    Signed-off-by: Jiaping Zeng <jpz@amazon.com>
    
    spotless apply
    
    Signed-off-by: Jiaping Zeng <jpz@amazon.com>
    
    test streaming from gradle
    
    Signed-off-by: Jiaping Zeng <jpz@amazon.com>
    
    fix http streaming handler + add tests
    
    Signed-off-by: Jiaping Zeng <jpz@amazon.com>
    
    fix http streaming tool use
    
    Signed-off-by: Jiaping Zeng <jpz@amazon.com>

--- common/src/test/java/org/opensearch/ml/common/settings/MLFeatureEnabledSettingTests.java
+++ common/src/test/java/org/opensearch/ml/common/settings/MLFeatureEnabledSettingTests.java
@@ -50,7 +50,8 @@ public class MLFeatureEnabledSettingTests {
                     MLCommonsSettings.ML_COMMONS_AGENTIC_MEMORY_ENABLED,
                     MLCommonsSettings.ML_COMMONS_INDEX_INSIGHT_FEATURE_ENABLED,
                     MLCommonsSettings.ML_COMMONS_STREAM_ENABLED,
-                    MLCommonsSettings.ML_COMMONS_MAX_JSON_SIZE
+                    MLCommonsSettings.ML_COMMONS_MAX_JSON_SIZE,
+                    MLCommonsSettings.ML_COMMONS_AG_UI_ENABLED
                 )
         );
         when(mockClusterService.getClusterSettings()).thenReturn(mockClusterSettings);
